8c72ae43fc1652514639a4824a4b70718271c06b
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-05-06 19:34:14 +0530

    media upload routing done and tested with django

--- router/upload.js
+++ router/upload.js
@@ -1,29 +1,75 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const fs = require("fs");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, `uploads`); // Save files to 'uploads/' directory
+  destination: function (req, file, cb) {
+    // Folder name based on field name
+    const fieldFolder = {
+      media: "uploads/media",
+      frame: "uploads/frames",
+    };
+
+    const folder = fieldFolder[file.fieldname] || "uploads/others";
+
+    // Ensure the folder exists
+    fs.mkdirSync(folder, { recursive: true });
+
+    cb(null, folder);
   },
-  filename: (req, file, cb) => {
-    const timestamp = Date.now(); // Unique timestamp
-    const ext = path.extname(file.originalname); // Keep original extension
-    const filename = `${timestamp}${ext}`; // Create unique filename
-    cb(null, filename); // Unique filename
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
   },
 });
 
 const upload = multer({ storage }); // For processing multipart/form-data
 
 // POST method to store frame in DB
-router.post("/", upload.single("media"), async (req, res) => {
+router.post("/media", upload.single("media"), async (req, res) => {
   try {
-    const filepath = req.file ? req.file.path : "uploads/placeholder-image.jpg";
-    res.status(201).json({ url: filepath });
+    const path = req.file ? req.file.path : "uploads/placeholder-image.jpg";
+    const urlPath = path.replace(/\\/g, "/");
+    console.log(req.file.path);
+    const url = `${req.protocol}://${req.get("host")}/${urlPath}`;
+    console.log(url);
+    res.status(200).json({ path: urlPath, url });
   } catch (error) {
     res.status(500).json({ message: "Error uploading!", error: error.message });
+  }
+});
+
+router.post("/frame", upload.single("frame"), async (req, res) => {
+  try {
+    const path = req.file ? req.file.path : "uploads/placeholder-image.jpg";
+    const urlPath = path.replace(/\\/g, "/");
+    console.log(req.file.path);
+    const url = `${req.protocol}://${req.get("host")}/${urlPath}`;
+    console.log(url);
+    res.status(200).json({ path: urlPath, url });
+  } catch (error) {
+    res.status(500).json({ message: "Error uploading!", error: error.message });
+  }
+});
+
+router.delete("/delete/:folder/:filename", (req, res) => {
+  const foldername = req.params.folder;
+  const filename = req.params.filename;
+  const filePath = path.join(
+    __dirname,
+    "..",
+    `uploads/${foldername}`,
+    filename
+  );
+  console.log("filePath", filePath);
+  if (fs.existsSync(filePath)) {
+    console.log("File exists, deleting...");
+    fs.unlinkSync(filePath);
+    return res.status(200).json({ message: "Deleted successfully" });
+  } else {
+    return res.status(404).json({ error: "File not found" });
   }
 });
 
