f103bb3febcc4edb06f7e082abdf45590ba17648
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-12-22 17:19:52 +0530

    configs updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,23 +3,21 @@ module.exports = {
     {
       name: "media-service-dev",
       script: "server.js",
-      cwd: "/var/www/dev/backend/media-service",
+      instances: "max",
+      exec_mode: "cluster",
       env: {
         NODE_ENV: "development",
         PORT: 4005,
-        ENV: "dev",
-        DEPLOY_BASE: "/var/www/dev/backend/media-service",
       },
     },
     {
       name: "media-service-prod",
       script: "server.js",
-      cwd: "/var/www/prod/backend/media-service",
+      instances: "max",
+      exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
         PORT: 4007,
-        ENV: "prod",
-        DEPLOY_BASE: "/var/www/prod/backend/media-service",
       },
     },
   ],
