941a9e11adb8d40253ae79895497a57df739b253
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-12-22 18:11:49 +0530

    log path updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,10 @@ module.exports = {
       script: "server.js",
       instances: "max",
       exec_mode: "cluster",
+      out_file: "/var/lib/jenkins/.pm2/logs/dev-media-service-out.log",
+      error_file: "/var/lib/jenkins/.pm2/logs/dev-media-service-error.log",
+      merge_logs: true,
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
       env: {
         NODE_ENV: "development",
         PORT: 4005,
@@ -16,6 +20,10 @@ module.exports = {
       script: "server.js",
       instances: "max",
       exec_mode: "cluster",
+      out_file: "/var/lib/jenkins/.pm2/logs/prod-media-service-out.log",
+      error_file: "/var/lib/jenkins/.pm2/logs/prod-media-service-error.log",
+      merge_logs: true,
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
       env: {
         NODE_ENV: "production",
         PORT: 4007,
