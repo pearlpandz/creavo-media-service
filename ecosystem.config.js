734877080751b6bad8ca055700fcf483b6c9bad8
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-12-22 15:20:50 +0530

    jenkins automation deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "creavo-media-service",
+      script: "server.js",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "development",
+        PORT: 4001,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 4003,
+      },
+    },
+  ],
+};
