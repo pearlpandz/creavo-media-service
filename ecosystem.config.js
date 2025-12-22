a697038b6e771e3a37c95596c8f05691960fae3e
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-12-22 17:17:35 +0530

    environment specific configs updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,25 @@
 module.exports = {
   apps: [
     {
-      name: "creavo-media-service",
+      name: "media-service-dev",
       script: "server.js",
-      instances: "max",
-      exec_mode: "cluster",
+      cwd: "/var/www/dev/backend/media-service",
       env: {
         NODE_ENV: "development",
         PORT: 4005,
+        ENV: "dev",
+        DEPLOY_BASE: "/var/www/dev/backend/media-service",
       },
-      env_production: {
+    },
+    {
+      name: "media-service-prod",
+      script: "server.js",
+      cwd: "/var/www/prod/backend/media-service",
+      env: {
         NODE_ENV: "production",
         PORT: 4007,
+        ENV: "prod",
+        DEPLOY_BASE: "/var/www/prod/backend/media-service",
       },
     },
   ],
